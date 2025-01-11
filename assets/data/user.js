ac47029d870e4f3893474ab26f223d54d8a7dba0
Abhishek Tripathi <tripathiji@Abhisheks-MacBook-Air.local>
2025-01-11 23:56:49 +0530

    dating app

---
+++ assets/data/user.js
@@ -0,0 +1,211 @@
+export default [
+  {
+    id: '1',
+    name: 'Jennifer',
+    age:22,
+    job:'Model Fashion',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeImage.png')
+  },
+  {
+    id: '2',
+    name: 'Pactrica',
+    age:23,
+    job:'Artist',
+    distance: '7 KM',
+    image: require('@/assets/images/HomeScreen/image-1.jpg')
+  },
+  {
+    id: '3',
+    name: 'Paul',
+    age:30,
+    job:'Police',
+    distance: '15 KM',
+    image: require('@/assets/images/HomeScreen/image-2.jpg')  },
+  {
+    id: '4',
+    name: 'Jhon',
+    age:22,
+    job:'Model ',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-3.jpg')  },
+  {
+    id: '5',
+    name: 'Abhay',
+    age:22,
+    job:'Model Fashion',
+    distance: '50 KM',
+    image: require('@/assets/images/HomeScreen/image-4.jpg')  },
+  {
+    id: '6',
+    name: 'Jeff',
+    age:22,
+    job:'Business Man',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-5.jpg')  },
+  {
+    id: '7',
+    name: 'Peter',
+    age:22,
+    job:'Land Owner',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-6.jpg')
+  },
+  {
+    id: '8',
+    name: 'Jennifer',
+    age:22,
+    job:'Model Fashion',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeImage.png')
+  },
+  {
+    id: '9',
+    name: 'Pactrica',
+    age:23,
+    job:'Artist',
+    distance: '7 KM',
+    image: require('@/assets/images/HomeScreen/image-1.jpg')
+  },
+  {
+    id: '10',
+    name: 'Paul',
+    age:30,
+    job:'Police',
+    distance: '15 KM',
+    image: require('@/assets/images/HomeScreen/image-2.jpg')  },
+  {
+    id: '11',
+    name: 'Jhon',
+    age:22,
+    job:'Model ',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-3.jpg')  },
+  {
+    id: '12',
+    name: 'Abhay',
+    age:22,
+    job:'Model Fashion',
+    distance: '50 KM',
+    image: require('@/assets/images/HomeScreen/image-4.jpg')  },
+  {
+    id: '13',
+    name: 'Jeff',
+    age:22,
+    job:'Business Man',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-5.jpg')  },
+  {
+    id: '14',
+    name: 'Peter',
+    age:22,
+    job:'Land Owner',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-6.jpg')
+  },
+  {
+    id: '15',
+    name: 'Jennifer',
+    age:22,
+    job:'Model Fashion',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeImage.png')
+  },
+  {
+    id: '16',
+    name: 'Pactrica',
+    age:23,
+    job:'Artist',
+    distance: '7 KM',
+    image: require('@/assets/images/HomeScreen/image-1.jpg')
+  },
+  {
+    id: '17',
+    name: 'Paul',
+    age:30,
+    job:'Police',
+    distance: '15 KM',
+    image: require('@/assets/images/HomeScreen/image-2.jpg')  },
+  {
+    id: '18',
+    name: 'Jhon',
+    age:22,
+    job:'Model ',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-3.jpg')  },
+  {
+    id: '19',
+    name: 'Abhay',
+    age:22,
+    job:'Model Fashion',
+    distance: '50 KM',
+    image: require('@/assets/images/HomeScreen/image-4.jpg')  },
+  {
+    id: '20',
+    name: 'Jeff',
+    age:22,
+    job:'Business Man',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-5.jpg')  },
+  {
+    id: '21',
+    name: 'Peter',
+    age:22,
+    job:'Land Owner',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-6.jpg')
+  },
+  {
+    id: '22',
+    name: 'Jennifer',
+    age:22,
+    job:'Model Fashion',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeImage.png')
+  },
+  {
+    id: '23',
+    name: 'Pactrica',
+    age:23,
+    job:'Artist',
+    distance: '7 KM',
+    image: require('@/assets/images/HomeScreen/image-1.jpg')
+  },
+  {
+    id: '24',
+    name: 'Paul',
+    age:30,
+    job:'Police',
+    distance: '15 KM',
+    image: require('@/assets/images/HomeScreen/image-2.jpg')  },
+  {
+    id: '25',
+    name: 'Jhon',
+    age:22,
+    job:'Model ',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-3.jpg')  },
+  {
+    id: '26',
+    name: 'Abhay',
+    age:22,
+    job:'Model Fashion',
+    distance: '50 KM',
+    image: require('@/assets/images/HomeScreen/image-4.jpg')  },
+  {
+    id: '27',
+    name: 'Jeff',
+    age:22,
+    job:'Business Man',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-5.jpg')  },
+  {
+    id: '28',
+    name: 'Peter',
+    age:22,
+    job:'Land Owner',
+    distance: '5 KM',
+    image: require('@/assets/images/HomeScreen/image-6.jpg')
+  },
+
+];
